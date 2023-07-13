f3a9f7730fe6bf223f931f58fd1116b67748d8c5
Adam Guerrero <adamguerrrero2021@gmail.com>
2023-07-13 00:59:17 -0400

    configuracion nuget

--- CapaDatos/CD_Carrito.cs
+++ CapaDatos/CD_Carrito.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using CapaEntidad;
-
 using System.Data.SqlClient;
 using System.Data;
 using System.Globalization;
